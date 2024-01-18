fd3950bd1c182e7fd0de8fac022d8b4b9a6b346a
Ermine.Krustane <Ermine.Krustane@rta.lv>
2024-01-18 15:57:24 +0200

    Izveidojam SetUpGame, kur aprakstijam Hero, Food, Enemy. 18.01.2024.

--- PacMan/Form1.cs
+++ PacMan/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,14 +24,20 @@ namespace PacMan
             this.BackColor = Color.Gray;
 
             Hero.BackColor = Color.Transparent;
-            Hero.Width = 30;
-            Hero.Height = 30;
+            Hero.Width = 35;
+            Hero.Height = 35;
+            Hero.SizeMode = PictureBoxSizeMode.StretchImage;
 
             Food.BackColor = Color.Transparent;
             Food.Width = 25;
             Food.Height = 25;
             Food.SizeMode = PictureBoxSizeMode.StretchImage;
             Food.Image = Properties.Resources.food_3;
+
+            Enemy.BackColor = Color.Transparent;
+            Enemy.Width = 30;
+            Enemy.Height = 30;
+            Enemy.SizeMode = PictureBoxSizeMode.StretchImage;
         }
     }
 }
